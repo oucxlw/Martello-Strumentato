99843d6afd9b79d555caef6e1c34ea28e3218b91
bolognesissimi <bolognesissimi@users.noreply.github.com>
2020-10-14 21:49:45 +0200

    Formattazione e Aggiustamento dei grafici

--- Funzioni-martello-strumentato/mechanical_impedence.m
+++ Funzioni-martello-strumentato/mechanical_impedence.m
@@ -11,7 +11,7 @@ tempstruct = struct('MI',A);
 % Accelerazione (A). Ciascuno di questi è una matrice le cui colonne sono
 % le registrazioni dei singoli colpi
 
-[r,~] = size(sng(1).F(:,1));
+[r,~] = size (sng(1).F(:,1));
 dt = 1/fs; time1 = 1000*(0:dt:r/fs-dt); %calcolo vettore tempo
 y_max = 0;
 x_max = 0;
@@ -21,13 +21,23 @@ for i=1:N
     % A causa del rumore di fondo si verifica un drift lineare che vogliamo
     % rimuovere. Quindi ci andiamo a prendere il valore di v_t dopo la fine del
     % picco, circa a metà take
-    x = round(length(v_t)/2); %prendo la x
-    y = v_t(x,:); %ricavo la y
+    x = round (length (v_t)/2); %prendo la x
+    y = v_t (x,:); %ricavo la y
     % costruisco un vettore costante proporzionale all inclinazione
-    v_drift = (ones(size(v_t)) .* y/x  );
+    v_drift = (ones (size (v_t)) .* y/x  );
     % Integro e ricostruisco il drift
     v_drift = cumtrapz (v_drift);
     v_t_corr = v_t - v_drift; %correggo v_t sottraendo il drift
+    
+    tempstruct(i).MI = max(sng(i).F) ./ max(v_t_corr); %calcolo di MI
+
+    %Cerco i limiti del grafico 702
+    if y_max < max (tempstruct(i).MI)
+        y_max = max (tempstruct(i).MI);
+    end
+    if x_max < length (tempstruct(i).MI)
+        x_max = length (tempstruct(i).MI);
+    end
     
     colpo = 2; %A titolo di esempio stampiamo la 2° misura
     
@@ -37,51 +47,56 @@ for i=1:N
     yyaxis right
     %plot(v_t(:,1))
     plot (time1, v_t_corr(:,colpo))
-    xlim ([15 40])
     
     figure (701), hold on, %stampo F vs F
     yyaxis left
     plot (time1, sng(i).F_filt(:,colpo)),
     yyaxis right
     plot (time1, v_t_corr(:,colpo))
-    xlim ([15 40])
     
-    tempstruct(i).MI = max(sng(i).F) ./ max(v_t_corr); %calcolo di MI
     % Plot degli indicatori MI dei singoli impatti
     figure (702), hold on %plot di MI
-    plot (10*log10(tempstruct(i).MI),'*', 'DisplayName', ['Sequenza n°', num2str(i)])
-    
-    if y_max < max(tempstruct(i).MI)
-        y_max = max(tempstruct(i).MI);
-    end
-    if x_max < length(tempstruct(i).MI)
-        x_max = length(tempstruct(i).MI);
-    end
+    plot (10*log10 (tempstruct(i).MI),'*', 'DisplayName', ['Sequenza n°', num2str(i)])
     
 end
+
+figure (700)
+yyaxis left
+ylabel ('Accelerazione','FontSize', 18)
+yyaxis right
+ylabel ('Velocità','FontSize', 18)
+xlim ([15 40])
+
+figure (701)
+yyaxis left
+ylabel ('Forza','FontSize', 18)
+yyaxis right
+ylabel ('Velocità','FontSize', 18)
+xlim ([15 40])
+
 figure (702), hold on %plot di MI
 grid on
 grid minor
 xlim ([0 x_max+1])
 xticks ( 1:x_max);
-ylim ([ 0 1.2*10*log10(y_max)])
-xlabel('Colpo progressivo', 'FontSize', 18)
-ylabel('Impedenza meccanica MI [dB @ 1Ns/m]', 'FontSize', 18)
-legend
+ylim ([0 1.2*10*log10(y_max)])
+xlabel ('Colpo progressivo', 'FontSize', 18)
+ylabel ('Impedenza meccanica MI [dB @ 1Ns/m]', 'FontSize', 18)
+legend ('FontSize', 12)
 
 MI_av = zeros(1,N);
 for i=1:N
-    MI_av(i)=mean(tempstruct(i).MI);
+    MI_av(i) = mean (tempstruct(i).MI);
 end
 
 figure (703), hold on
-bar (1, 10*log10(MI_av'));%,'DisplayName', ['Sequenza n°', num2str(i)]);
+bar (1, 10*log10 (MI_av'));%,'DisplayName', ['Sequenza n°', num2str(i)]);
 xticks ([])
-ylabel('Impedenza meccanica MI'' [dB @ 1Ns/m]', 'FontSize', 18)
+ylabel ('Impedenza meccanica MI'' [dB @ 1Ns/m]', 'FontSize', 18)
 grid on
 grid minor
-legend( 'FontSize', 12)
-saveas(gcf, ['Impedenza_meccanica-M_Li_2016.fig'])
-exportgraphics(gcf,'Impedenza_meccanica-M_Li_2016.pdf', 'BackgroundColor', 'none', 'Resolution', 300) 
+legend ('FontSize', 12)
+saveas (gcf, 'Impedenza_meccanica-M_Li_2016.fig')
+exportgraphics (gcf,'Impedenza_meccanica-M_Li_2016.pdf', 'BackgroundColor', 'none', 'Resolution', 300) 
 
 end
